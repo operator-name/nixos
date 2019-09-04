cb3899840450551a5695bb248cab7062d7a61ad5
operator.name <operator.name@protonmail.com>
2019-09-04 16:23:19 +0100

    Pin update channel to 19.03

--- nix.nix
+++ nix.nix
@@ -2,8 +2,9 @@
 
 {
   nix.autoOptimiseStore = true;
-#   nix.gc.automatic = true;
+  # nix.gc.automatic = true;
 
   system.autoUpgrade.enable = true;
+  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-19.03";
   system.nixos.tags = [ "gnome3" ];
 }
