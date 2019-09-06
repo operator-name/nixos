168128d451e8e15caded445390ee50c24f61f338
operator.name <operator.name@protonmail.com>
2019-09-06 15:28:02 +0100

    Enable trackpoint with default acceleration

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,8 @@
   environment.systemPackages = with pkgs; [
   ];
 
+  hardware.trackpoint.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
